513ac092208d0f948a52da69ca0a80875b4a237c
Ksenia Simonova <ksansi@yandex.ru>
2020-10-13 01:28:32 +0500

    initial commit

--- lab1_2/main.c
+++ lab1_2/main.c
@@ -1,1 +1,30 @@
+#include <locale.h>
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+double y(double x)
+{
+    if (x<=-1)
+    {
+        return -1./(x*x);
+    }
+    else if (x>-1 && x<=2)
+    {
+        return x*x;
+    }
+    else
+    {
+        return 4;
+    }  
+}
+
+int main()
+{
+    double a;
+    printf("Введите действующее значение х: \n");
+    scanf("%lf", &a);
+    double function = y(a);
+    printf("f(%lf) = %lf\n", a, function);
+    getchar();
+}
